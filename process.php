eed1e47a530ce102da1fb719536ab6402f65f149
Sanwebe <saaraan@gmail.com>
2016-07-23 15:20:26 +0530

    Update process.php

--- process.php
+++ process.php
@@ -5,9 +5,9 @@ $config["generate_thumbnails"]			= true;
 $config["image_max_size"] 				= 500; //Maximum image size (height and width)
 $config["thumbnail_size"]  				= 200; //Thumbnails will be cropped to 200x200 pixels
 $config["thumbnail_prefix"]				= "thumb_"; //Normal thumb Prefix
-$config["destination_folder"]			= 'D:/Websites/ajax-image-upload/uploads/'; //upload directory ends with / (slash)
-$config["thumbnail_destination_folder"]	= 'D:/Websites/ajax-image-upload/uploads/'; //upload directory ends with / (slash)
-$config["upload_url"] 					= "http://localhost/ajax-image-upload/uploads/"; 
+$config["destination_folder"]			= 'home/Website/ajax-image-upload/uploads/'; //upload directory ends with / (slash)
+$config["thumbnail_destination_folder"]	= 'home/Website/ajax-image-upload/uploads/'; //upload directory ends with / (slash)
+$config["upload_url"] 					= "http://website/ajax-image-upload/uploads/"; 
 $config["quality"] 						= 90; //jpeg quality
 $config["random_file_name"]				= true; //randomize each file name
 
